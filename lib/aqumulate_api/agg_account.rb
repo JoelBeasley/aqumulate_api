b3589b31a838551cedbb216efc35e3c3bf63b392
Kevin Pheasey <kevin@kpheasey.com>
2016-09-09 12:30:09 -0400

    Standardize api method declaration using a hash.

--- lib/aqumulate_api/agg_account.rb
+++ lib/aqumulate_api/agg_account.rb
@@ -1,63 +1,30 @@
 module AqumulateAPI
   class AggAccount
-
     RESOURCE = 'AggAccount'
+  end
 
-    def self.add_advisor(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/AddAdvisor", body)
-    end
-    
-    def self.advisor_sign_on(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/AdvisorSignOn", body)
-    end
-    
-    def self.get_advisor_by_id(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/GetAdvisorById", body)
-    end
-    
-    def self.update_advisor(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/UpdateAdvisor", body)
-    end
-    
-    def self.delete_advisor(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/DeleteAdvisor", body)
-    end
+  AggAccount.singleton_class.class_eval do
+    methods = {
+        add_advisor: { resource: 'AddAdvisor' },
+        advisor_sign_on: { resource: 'AdvisorSignOn' },
+        get_advisor_by_id: { resource: 'GetAdvisorById' },
+        update_advisor: { resource: 'UpdateAdvisor' },
+        delete_advisor: { resource: 'DeleteAdvisor' },
+        advisor_harvest_accounts: { resource: 'AdvisorHarvestAccounts' },
+        advisor_harvest_account_status_check: { resource: 'AdvisorHarvestAccountStatusCheck' },
+        advisor_get_aggregate_account: { resource: 'AdvisorGetAggregateAccount' },
+        advisor_agg_get_account_for_fi: { resource: 'AdvisorAggGetAccountForFI' },
+        link_account_to_client: { resource: 'LinkAccountToClient' },
+        advisor_get_transactions_by_date: { resource: 'AdvisorGetTransactionsByDate' },
+        advisor_search_financial_institution: { resource: 'AdvisorSearchFinancialInstitution' },
+        advisor_get_financial_institution: { resource: 'AdvisorGetFinancialInstitution' },
+        advisor_add_account: { resource: 'AdvisorAddAccount' }
+    }
 
-    def self.advisor_harvest_accounts(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/AdvisorHarvestAccounts", body)
+    methods.each do |method, definition|
+      define_method(method) do |body, session = AqumulateAPI.session|
+        session.api_request("#{self::RESOURCE}/#{definition[:resource]}", body)
+      end
     end
-
-    def self.advisor_harvest_account_status_check(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/AdvisorHarvestAccountStatusCheck", body)
-    end
-
-    def self.advisor_get_aggregate_account(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/AdvisorGetAggregateAccount", body)
-    end
-
-    def self.advisor_agg_get_account_for_fi(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/AdvisorAggGetAccountForFI", body)
-    end
-
-    def self.link_account_to_Client(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/LinkAccountToClient", body)
-    end
-
-    def self.advisor_get_transactions_by_date(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/ADvisorGetTransactionsByDate", body)
-    end
-
-    def self.advisor_get_financial_institution(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/AdvisorGetFinancialInstitution", body)
-    end
-
-    def self.advisor_search_financial_institution(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/AdvisorSearchFinancialInstitution", body)
-    end
-
-    def self.advisor_add_account(body, session = AqumulateAPI.session)
-      session.api_request("#{RESOURCE}/AdvisorAddAccount", body)
-    end
-
   end
 end
